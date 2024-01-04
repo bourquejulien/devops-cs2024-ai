58984cdd490b6f5b41ee6ffcee1e9e65fcd9b1c5
Julien Bourque <julien@brqu.me>
2024-01-04 10:44:24 -0500

    Added team cluster

--- infra/module/main.tf
+++ infra/module/main.tf
@@ -24,6 +24,23 @@ resource "azurerm_kubernetes_cluster" "ai_cluster" {
   }
 }
 
+resource "azurerm_kubernetes_cluster" "team_cluster" {
+  name                = "team-cluster"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  dns_prefix          = "aicluster"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_B2s"
+  }
+
+  identity {
+      type = "SystemAssigned"
+  }
+}
+
 resource "azurerm_storage_account" "table_account" {
   name                     = "ta${lower(random_id.storage_account.hex)}"
   resource_group_name      = azurerm_resource_group.rg.name
