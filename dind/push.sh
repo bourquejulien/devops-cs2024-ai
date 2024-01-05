66a6376dafc5628dab4f83bae19b125cf4eadfae
Julien Bourque <julien@brqu.me>
2024-01-04 20:13:42 -0500

    Added custom pipeline docker image

---
+++ dind/push.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+set -e
+
+if [[ $EUID -ne 0 ]]; then
+   echo "Please run as root"
+   exit 1
+fi
+
+FORCE_LOGIN=false
+REGISTRY="index.docker.io/v1"
+BASE_PATH="brqu"
+TAG="latest"
+IMAGE="docker-az"
+
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    -f)
+      FORCE_LOGIN=true
+      shift
+      ;;
+    *)
+      echo "Unknown option $1"
+      exit 1
+      ;;
+  esac
+done
+
+if $FORCE_LOGIN || ! grep --quiet $REGISTRY ~/.docker/config.json ; then
+    echo "Enter Docker username:"
+    read USER
+    echo "Enter Docker password/token:"
+    read -s PASSWORD
+    
+    echo $PASSWORD | docker login -u $USER --password-stdin $REGISTRY
+    
+    if ! [ $? -eq 0 ] ; then
+       echo "Login failed"
+       exit 1
+    fi
+else
+    echo "Already logged in $REGISTRY"
+fi
+
+printf "\n----------------------\nNow building $BASE_PATH/$IMAGE:$TAG images:\n----------------------\n"
+DOCKER_BUILDKIT=1 docker build -t "$BASE_PATH/$IMAGE:$TAG" .
+
+printf "\n----------------------\nNow pushing $BASE_PATH/$IMAGE:$TAG images:\n----------------------\n"
+docker push "$BASE_PATH/$IMAGE:$TAG"
